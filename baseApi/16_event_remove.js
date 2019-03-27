a6a74d151540dcf5cc307e430c1179df5d7a31df
陈喆 <307400468@qq.com>
2019-03-27 12:12:03 +0800

    event

--- baseApi/16_event_remove.js
+++ baseApi/16_event_remove.js
@@ -18,10 +18,22 @@ ce.on('test', fn1);
 ce.on('test', fn2);
 
 setInterval(() => {
-  ce.emit('test');
+  // ce.emit('test');
 }, 500);
 
 setTimeout(() => {
   ce.removeListener('test', fn1);
   // ce.removeAllListeners('test');
 }, 1500);
+
+function pong (a) {
+  console.log('pong');
+  console.log(a);
+}
+
+ce.on('ping', pong);
+ce.once('ping', pong);
+// ce.removeListener('ping', pong); // 移除once，最近添加的
+
+ce.emit('ping', 'a');
+ce.emit('ping', 'b');
